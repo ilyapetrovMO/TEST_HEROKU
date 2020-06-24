b1a57fbba598f969e3d3936c52b02b024963fa71
Ilya Petrov <ilyapetrov.mosobl@gmail.com>
2020-06-24 17:03:27 +0300

    tool manifest

--- Startup.cs
+++ Startup.cs
@@ -67,7 +67,7 @@ namespace HerokuTest
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             app.UseHttpsRedirection();
